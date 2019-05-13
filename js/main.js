5dd3e8e5b3b267b5914e17f2ccb4ec7bf499ff4d
Raghu prasad <csraghuprasad@gmail.com>
2019-05-13 17:15:45 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+angular.module("editor", ['htmlEditor', "ui.router"])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise("/home");
 
+        var homeState = {
+            name: 'home',
+            url: '/',
+            templateUrl: "templates/home.html"
+        };
+
+        var installationState = {
+            name: 'installation',
+            url: '/installation',
+            templateUrl: "templates/installation.html"
+        };
+        var demoState = {
+            name: 'demo',
+            url: '/demo',
+            templateUrl: "templates/demo.html",
+            controller:'demoCtrl'
+        };
+
+        $stateProvider.state(homeState);
+        $stateProvider.state(installationState);
+        $stateProvider.state(demoState);
+
+        $locationProvider.html5Mode(true);
+    }).controller('mainCtrl', ['$scope', function ($scope) {
+
+    }]).controller('demoCtrl', ['$scope', function ($scope) {
+        $scope.htmlContent = '<h3>Add your content here.</h3><p>Enjoy!</p>';
+    }]);
